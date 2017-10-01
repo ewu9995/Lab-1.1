34b3e853f980e692a2949c6f7b0662d3ebffa473
ewu9995 <32362354+ewu9995@users.noreply.github.com>
2017-10-01 10:25:23 -0400

    Update ControlStructures.java

--- src/ControlStructures.java
+++ src/ControlStructures.java
@@ -2,22 +2,39 @@
 public class ControlStructures {
 	public static void main(String [] args) {
 		primePrinter();
+		System.out.print("\n");
 		fooBarBaz();
-		System.out.println("We're done!");
+		System.out.print("\n");
+		System.out.print(" " + "We're done!");
 	}
 	
 	public static void primePrinter() {
-		int number = 459;
-		for (int x = 2; 2 * x <= number; x++) {
-			if (((x++) % 2) == 0) {
-				System.out.print(" " + x);
-				
+		int x = 0;
+		int number = 0;
+		int count=0;
+		String primeNumber = "";
+		for (x = 1;  x <= 229; x++) {
+			int counter=0;
+			for (number=x; number>=1; number--) {
+			
+			if (x % number == 0) {
+				counter = counter +1;
+				}
 			}
-	   }
-	
+			if (counter==2) {
+				System.out.print(" " +x);
+				count ++;
+			if (count==10) {
+				System.out.print("\n");
+				count =0;
+			}
+			primeNumber = primeNumber + x + " ";
+			}
+		}
 	}
 	
-
+	
+	
 	
 	public static void fooBarBaz() {
 		
@@ -38,19 +55,21 @@ public class ControlStructures {
 			} else if ((x % 3 == 0) && (x % 7 ==0) && (x % 10 == 0)) {
 				System.out.print(" "+"fooBarbaz");
 			} else {
-				System.out.print(" " + x);
+				System.out.print(" " +x);
 			}	
 		}
 	}
 	
 	
-	
-	public static void isPrime () {
-		for (int x = 2; 2 *x <= 459; x++) {
-			if (((x++) % 2) == 0) {
-				System.out.println(x);
+	// Not used
+	public static boolean isPrime(int x) {
+		int number = 459;
+		for  (x = 2; 2 *x <= number; x++) {
+			if (number % x == 0) {
+				return false;
 			}
 		}
+		return true;
 	}
 
 
